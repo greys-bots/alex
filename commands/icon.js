db739f7502b43dc04125422ff79107d9f15dacf2
xgreyskiesx <greysdawn@gmail.com>
2019-10-11 00:40:10 -0400

    Fix icon bug the rest of the way

--- commands/icon.js
+++ commands/icon.js
@@ -11,7 +11,7 @@ module.exports = {
 		else if(msg.attachments[0]) url = msg.attachments[0].url;
 		else return msg.channel.createMessage("Please provide a url or attach an image");
 
-		var res = await bot.utils.updateServer(bot, msg.guild.id, args[0], 'pic_url', args.slice(1).join(" "));
+		var res = await bot.utils.updateServer(bot, msg.guild.id, args[0], 'pic_url', url);
 		var res2 = await bot.utils.updatePosts(bot, msg.guild.id, args[0]);
 		if(res && res2) {
 			msg.channel.createMessage('Icon updated!');
