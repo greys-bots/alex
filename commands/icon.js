e84aa686efe3ac5ced7898c9a905d2812ca894e6
xgreyskiesx <greysdawn@gmail.com>
2019-10-11 00:26:59 -0400

    Allow image uploads for icons

--- commands/icon.js
+++ commands/icon.js
@@ -1,11 +1,17 @@
 module.exports = {
 	help: ()=> "Edits a server's icon.",
-	usage: ()=> [" [id] [url] - Sets server's icon to given url"],
+	usage: ()=> [" [id] <url> - Sets server's icon. If no url is given, uses the first attached image"],
 	execute: async (bot, msg, args) => {
 		var guild = await bot.utils.getServer(bot, msg.guild.id, args[0]);
 		if(!guild) return msg.channel.createMessage('Server not found');
 
-		var res = await bot.utils.updateServer(bot, msg.guild.id, args[0], 'icon', args.slice(1).join(" "));
+		var url;
+
+		if(args[1]) url = args[1];
+		else if(msg.attachments[0]) url = msg.attachments[0].url;
+		else return msg.channel.createMessage("Please provide a url or attach an image");
+
+		var res = await bot.utils.updateServer(bot, msg.guild.id, args[0], 'pic_url', args.slice(1).join(" "));
 		var res2 = await bot.utils.updatePosts(bot, msg.guild.id, args[0]);
 		if(res && res2) {
 			msg.channel.createMessage('Icon updated!');
